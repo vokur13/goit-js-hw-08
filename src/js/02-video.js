7b693e23e8b6eb159731b524a58f406472b4cf2b
vokur13 <vokur13@gmail.com>
2022-08-21 14:29:23 +0300

    package.json fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import VimeoPlayer from '@vimeo/player';
 import LodashThrottle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
