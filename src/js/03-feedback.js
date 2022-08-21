ef2963e86c23b0680291db1d3f8f71ea248c552b
vokur13 <vokur13@gmail.com>
2022-08-21 14:04:26 +0300

    tasks-01, 03 fixed

---
+++ src/js/03-feedback.js
@@ -0,0 +1,37 @@
+import LodashThrottle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', LodashThrottle(onInput, 500));
+
+getStorageOutput();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  e.target.reset();
+}
+function onInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+function getStorageOutput() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  const parseData = JSON.parse(savedData);
+  if (parseData) {
+    refs.email.value = parseData.email;
+    refs.textarea.value = parseData.message;
+  } else {
+    refs.email.value = '';
+    refs.textarea.value = '';
+  }
+}
