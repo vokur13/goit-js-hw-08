a852f76bc287ae17fb575f44f6abb18da3736b74
vokur13 <vokur13@gmail.com>
2022-09-10 18:43:32 +0300

    fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -35,91 +35,22 @@ getStorageOutput();
 function onInput(e) {
   const key = e.target.name;
   const value = e.target.value;
-
+  formData[key] = value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  for (const key in formData) {
-    if (!formData.hasOwnProperty(key)) {
-      formData[key] = value;
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-      return;
-    }
-  }
-  const savedData = localStorage.getItem(STORAGE_KEY);
-  if (savedData) {
-    const parseData = JSON.parse(savedData);
-    parseData = { key: value };
-    formData = parseData;
-    //     FormData();
-    formData = { key: value };
-    console.log('parseData', parseData);
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-    return;
-  }
-
-  //   if (!formData.hasOwnProperty([key])) {
-  //     formData[key] = value;
-
-  //   }
-
-  return;
 }
 
-//
-const formData = new FormData(refs.form);
-formData.forEach((name, value) => {
-  console.log(name);
-  console.log(value);
-});
-console.log(formData);
-// ---------------------------
-// for (const key in userDeveloper) {
-//   if (userDeveloper.hasOwnProperty(key)) {
-//     const element = userDeveloper[key];
-//     console.log('element', element);
+// function getStorageOutput() {
+//   const savedData = localStorage.getItem(STORAGE_KEY);
+//   if (savedData) {
+//     const parseData = JSON.parse(savedData);
+//     if (parseData.hasOwnProperty('email')) {
+//       refs.email.value = parseData.email;
+//     }
+//     if (parseData.hasOwnProperty('message')) {
+//       refs.textarea.value = parseData.message;
+//     }
 //   }
 // }
-
-// for (const key in userDeveloper) {
-//   if (userDeveloper.hasOwnProperty(key)) {
-//     console.log('key', key);
-//   }
-// //
-
-//         //Object.keys повертає масив власних ключів
-
-// const keys = Object.keys(userDeveloper);
-// console.log('keys', keys);
-
-// const value = Object.values(userDeveloper);
-// console.log('value', value);
-
-// for (const key of keys) {
-//   console.log(userDeveloper[key]);
-//   console.log(key);
-// }
-
-//         //
-
-//         const name = 'mango';
-//         const email = 'email@mail.com'
-//         const user = { name, email }
-//         console.log(user);
-
-//         //
-// ---------------------------
-
-function getStorageOutput() {
-  const savedData = localStorage.getItem(STORAGE_KEY);
-  if (savedData) {
-    const parseData = JSON.parse(savedData);
-    if (parseData.hasOwnProperty('email')) {
-      refs.email.value = parseData.email;
-    }
-    if (parseData.hasOwnProperty('message')) {
-      refs.textarea.value = parseData.message;
-    }
-  }
-}
 function onFormSubmit(e) {
   e.preventDefault();
   const storedData = localStorage.getItem(STORAGE_KEY);
