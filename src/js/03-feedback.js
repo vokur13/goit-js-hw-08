01acbbe08ebb15f5a7f31a09376dd4957e629c6a
vokur13 <vokur13@gmail.com>
2022-09-22 13:37:42 +0300

    feedback form finally fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,61 +1,37 @@
 import LodashThrottle from 'lodash.throttle';
+import { save, load, remove } from './storage';
 
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
 
 const refs = {
   form: document.querySelector('.feedback-form'),
-  email: document.querySelector('.feedback-form input'),
-  textarea: document.querySelector('.feedback-form textarea'),
 };
 
-refs.form.addEventListener('input', LodashThrottle(onInput, 500));
+refs.form.addEventListener('input', LodashThrottle(onFormInput, 500));
 refs.form.addEventListener('submit', onFormSubmit);
 
-getStorageOutput();
+initPage();
 
-// function onInput(e) {
-//   const key = e.target.name;
-//   const value = e.target.value;
-//   formData[key] = value;
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// }
-
-// function onInput(e) {
-//   const key = e.target.name;
-//   const value = e.target.value;
-//   if (key === 'email' || key === 'message') {
-//     formData.email = refs.email.value;
-//     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-//     formData.message = refs.textarea.value;
-//     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-//   }
-// }
-
-function onInput(e) {
-  const key = e.target.name;
-  const value = e.target.value;
-  formData[key] = value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+function onFormInput(e) {
+  const { name, value } = e.target;
+  let formData = load(STORAGE_KEY);
+  formData = formData ? formData : {};
+  formData[name] = value;
+  save(STORAGE_KEY, formData);
 }
 
-// function getStorageOutput() {
-//   const savedData = localStorage.getItem(STORAGE_KEY);
-//   if (savedData) {
-//     const parseData = JSON.parse(savedData);
-//     if (parseData.hasOwnProperty('email')) {
-//       refs.email.value = parseData.email;
-//     }
-//     if (parseData.hasOwnProperty('message')) {
-//       refs.textarea.value = parseData.message;
-//     }
-//   }
-// }
+function initPage() {
+  const parseData = load(STORAGE_KEY);
+  if (parseData) {
+    const savedData = Object.entries(parseData);
+    savedData.forEach(([name, value]) => {
+      refs.form.elements[name].value = value;
+    });
+  }
+}
+
 function onFormSubmit(e) {
   e.preventDefault();
-  const storedData = localStorage.getItem(STORAGE_KEY);
-  const printData = JSON.parse(storedData);
-  console.log(printData);
-  localStorage.removeItem(STORAGE_KEY);
-  e.target.reset();
+  remove(STORAGE_KEY);
+  e.currentTarget.reset();
 }
